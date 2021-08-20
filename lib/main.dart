56b61c90308d6e3505dd127a27aa2afde850e2d5
Alexandre dos Santos Rodrigues <Alexandre.santos.cel@gmail.com>
2021-08-19 21:19:23 -0300

    agora vai

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/model/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -11,6 +12,8 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomeApp extends StatelessWidget {
+  final titleControler = TextEditingController();
+  final valueController = TextEditingController();
   final _transactions = [
     Transaction(
       id: '1',
@@ -21,7 +24,7 @@ class MyHomeApp extends StatelessWidget {
     Transaction(
       id: '2',
       title: 'conta de luz',
-      value: 180.21,
+      value: 180.2,
       date: DateTime.now(),
     ),
   ];
@@ -32,7 +35,7 @@ class MyHomeApp extends StatelessWidget {
           title: Text('Despesas Pessoais'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
@@ -49,18 +52,76 @@ class MyHomeApp extends StatelessWidget {
                         child: Row(
                           children: [
                             Container(
-                              child: Text(tr.value.toString()),
+                              margin: EdgeInsets.symmetric(
+                                horizontal: 15,
+                                vertical: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                color: Colors.purple,
+                                width: 2,
+                              )),
+                              padding: EdgeInsets.all(10),
+                              child: Text(
+                                'R\S ${tr.value.toStringAsFixed(2)}',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                    color: Colors.purple),
+                              ),
                             ),
                             Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(tr.title),
-                                Text(tr.date.toString()),
+                                Text(
+                                  tr.title,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      color: Colors.purple),
+                                ),
+                                Text(
+                                  DateFormat('dd MM y').format(tr.date),
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.grey[700]),
+                                ),
                               ],
                             )
                           ],
                         ),
                       ))
                   .toList(),
+            ),
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: titleControler,
+                      decoration: InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    TextField(
+                      controller: valueController,
+                      decoration: InputDecoration(labelText: 'Valor em R\$'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Nova Transação',
+                              style: TextStyle(color: Colors.purple),
+                            )),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ));
